477b2123f8432e6a66aa69e41eb8595820c52bc9
Pratik Sukale <vjkiran@MacBookPro.lan>
2025-03-07 14:50:04 +0530

    Some refactering and Project theme, structure setup

---
+++ Server/routes/claudinary_config.py
@@ -0,0 +1,8 @@
+import cloudinary
+
+cloudinary.config(
+    cloud_name="dzmqbmgqc",
+    api_key="453329355371866",
+    api_secret="rK06QhaPdgDQPGJtc60wu8a2jVg",  # Click 'View API Keys' above to copy your API secret
+    secure=True,
+)
